1cf716a92ffe2024afa742c740373068ef312a60
Bodil Hundevad <bodil@hundevad.de>
2023-03-10 17:56:59 +0100

    Add email factory
    Add factory for email with sample content to generate new emails quickly.
    Email content is primarily targeted for job applicants.
    It contains details regarding job offer and communication preferences.

---
+++ spec/factories/email.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :email do
+    email_content {
+      'Hi Bodil,\n' \
+      'Thank you for applying at Tractive! Since we’re all about open communication and quick feedback, we thought we’d give you the good news right away - we’d like to proceed further with your application. \n' \
+      'To do so, we need the following from you:\n' \
+      'We would like to ask you to fill in our technology screening in order to give us a better overview of your programming skills. Please be honest with your self-evaluation!\n' \
+      'Just so you know, we do not offer full remote working at Tractive. We believe in the power of doing things all together, in one place. However in addition to our flexible working hours, we also offer a hybrid 2:2 home office model (2 home office days / 2 office days) to ensure that you are spatially independent. Is commuting to Linz/Austria for at least 2 office days a week an option for you?\n' \
+      'Here is the link to access your application and personal info. You can also get some exclusive insights into the application process at Tractive here.\n' \
+      '\n' \
+      'Looking forward to hearing from you soon! Have a great day!\n' \
+      '\n' \
+      'Best Regards,\n' \
+      'Romana Haspelhuber'
+    }
+  end
+end

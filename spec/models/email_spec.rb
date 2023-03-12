9eb80cc28ba37ede51be7326fccad7664200f439
Bodil Hundevad <bodil@hundevad.de>
2023-03-12 17:20:21 +0100

    Add specs for Email model
    Describe creation of an Email model instance,
    allowing creation of email without received email or
    description, or disallowing creation of empty emails.

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Email, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "creation" do
+    let(:email_received_empty) { build :email, :received_empty }
+    let(:email_description_empty) { build :email, :description_empty }
+    let(:email_empty) { build :empty }
+
+    it "allows creation of email without received email" do
+      expect(email_received_empty).to be_valid
+    end
+
+    it "allows creation of email without description" do
+      expect(email_description_empty).to be_valid
+    end
+
+    it "does not allow creation of email without received or description" do
+      expect(email_empty).not_to be_valid
+    end
+  end
 end

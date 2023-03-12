9ee87c6189bf6a9b89f2f07786411888fe2920d9
Bodil Hundevad <bodil@hundevad.de>
2023-03-12 13:48:38 +0100

    Add system tests for generating email response
    Add system tests for generating email response providing email, description and tonality, plus a pending test for not allowing an email response without providing email or description.

--- spec/system/generate_email_spec.rb
+++ spec/system/generate_email_spec.rb
@@ -2,12 +2,22 @@ require "rails_helper"
 
 RSpec.describe "generating an email response", type: :system do
 
-  it "allows a user to generate an email response to their pasted email", pending: "Describing Unit Tests" do
-    email_content = FactoryBot.create(:email)
+  it "allows a user to generate an email response providing email, description, and tonality" do #pending: "Describing Unit Tests"
+    email = FactoryBot.create(:email)
     visit root_path
-    fill_in "Email", with: email_content
+    expect(page).to have_field("email_received")
+    expect(page).to have_field("email_description")
+    expect(page).to have_select("email_tonality"), options: ["business", "business-casual", "casual"]
+
+    fill_in "email_received", with: email.received
+    fill_in "email_description", with: email.description
+    fill_in "email_tonality", with: email.tonality
+
     click_on("Generate email")
     expect(page).to have_selector("#email_response")
     expect(page).to have_current_path(root_path)
+  end
+
+  it "does not allow a user to generate an email response without providing email or description", pending: "TBD" do
   end
 end

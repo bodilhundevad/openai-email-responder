7151c3f5668c43e0858288c880a680039ca3ccab
Bodil Hundevad <bodil@hundevad.de>
2023-03-12 18:46:45 +0100

    Add tonality validation and default value
    Add `tonality` field validation and default
    value to the Email model to ensure all
    emails have a tonality set.

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,9 @@
 class Email < ApplicationRecord
   validates :received, presence: true, unless: ->(email) { email.description.present? }
   validates :description, presence: true, unless: ->(email) { email.received.present? }
+  validates :tonality, presence: true
+
+  before_validation do
+    self.tonality = "casual" if tonality.blank?
+  end
 end

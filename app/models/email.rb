901adfcb1bb8a77dfde706a9eb269f71eb728ed1
Bodil Hundevad <bodil@hundevad.de>
2023-03-12 17:19:34 +0100

    Add validations for Email model
    Validate presence of received and description fields
    according to one or the other being present

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,4 @@
 class Email < ApplicationRecord
+  validates :received, presence: true, unless: ->(email) { email.description.present? }
+  validates :description, presence: true, unless: ->(email) { email.received.present? }
 end

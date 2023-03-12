96db6766f5f6d9556808227d1feb84affdf41198
Bodil Hundevad <bodil@hundevad.de>
2023-03-12 13:45:48 +0100

    Configure test framework and specs in rails: - Change test framework from Test::Unit to RSpec - Specify fixture replacement with FactoryBot - Define controller, request, feature, mailer, model, observer,   routing and view specs

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,18 @@ module OpenaiEmailResponder
     config.generators do |generate|
       generate.assets false
       generate.helper false
-      generate.test_framework :test_unit, fixture: false
+      generate.test_framework :rspec
+      g.fixture_replacement :factory_bot
+      g.factory_bot dir: 'spec/factories'
+      g.controller_specs false
+      g.request_specs true
+      g.helper_specs false
+      g.feature_specs true
+      g.mailer_specs true
+      g.model_specs true
+      g.observer_specs false
+      g.routing_specs false
+      g.view_specs false
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0

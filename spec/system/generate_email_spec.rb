1205123a4bfd39cbc46493924d3daf25e5adf65c
Bodil Hundevad <bodil@hundevad.de>
2023-03-13 20:03:12 +0100

    Refactor: Add VCR and aggregate failures to generate_email_spec
    Add VCR and aggregate failures to spec/system/generate_email_spec.rb to improve stability
    and testability of generating email response feature.

--- spec/system/generate_email_spec.rb
+++ spec/system/generate_email_spec.rb
@@ -2,16 +2,13 @@ require "rails_helper"
 
 RSpec.describe "generating an email response", type: :system do
   let(:email) { build_stubbed :email, :tonality_business }
-  it "allows a user to generate an email response providing email, description, and tonality" do #pending: "Describing Unit Tests"
+  it "allows a user to generate an email response providing email, description, and tonality", :vcr, :aggregate_failures do #pending: "Describing Unit Tests"
     visit root_path
-
     fill_in "email_received", with: email.received
     fill_in "email_description", with: email.description
     choose(email.tonality)
     click_on("Create Email")
-
-    expect(page).to have_selector("#email_response")
-    expect(page).to have_current_path(root_path)
+    expect(page).to have_selector(".email_response")
   end
 
   it "does not allow a user to generate an email response without providing email or description", pending: "TBD" do

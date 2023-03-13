37f53b57084b0d37201a3dfb3878a043a689f8a7
Bodil Hundevad <bodil@hundevad.de>
2023-03-13 20:04:29 +0100

    Refactor email_generator.rb to improve readability
    Refactor email_generator.rb to improve code structure
    and readability by introducing client, call and
    get_openai_parameters methods to handle
    OpenAI interaction and prompt generation logic.

--- app/services/email_generator.rb
+++ app/services/email_generator.rb
@@ -4,6 +4,28 @@ class EmailGenerator
   end
 
   def email_message
-    "Hello, here is the response."
+    call.dig("choices", 0, "text")
+  end
+
+  def client
+    OpenAI::Client.new
+  end
+
+  def call #calls api endpoint to get the response
+    parameters = get_openai_parameters(@email)
+    client.completions(parameters: parameters)
+  end
+
+  def get_openai_parameters(email)
+    {
+      model: "text-davinci-001",
+      prompt: generate_prompt(email),
+      max_tokens: 150
+    }
+  end
+
+  # Logic for prompt generation based on email instance
+  def generate_prompt(email)
+    "Generate an email example for sending a job application"
   end
 end

25ede460b23cfe35d22115d0cee86c62a3ea432a
Bodil Hundevad <bodil@hundevad.de>
2023-03-13 10:16:58 +0100

    Refactor forms for email response: - Use choose instead of fill_in for tonality selection - Add create button for email
    Refactor: replace fill_in with choose for tonality selection and add create button

--- spec/system/generate_email_spec.rb
+++ spec/system/generate_email_spec.rb
@@ -1,19 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "generating an email response", type: :system do
-
+  let(:email) { build_stubbed :email, :tonality_business }
   it "allows a user to generate an email response providing email, description, and tonality" do #pending: "Describing Unit Tests"
-    email = FactoryBot.create(:email)
     visit root_path
-    expect(page).to have_field("email_received")
-    expect(page).to have_field("email_description")
-    expect(page).to have_select("email_tonality"), options: ["business", "business-casual", "casual"]
 
     fill_in "email_received", with: email.received
     fill_in "email_description", with: email.description
-    fill_in "email_tonality", with: email.tonality
+    choose(email.tonality)
+    click_on("Create Email")
 
-    click_on("Generate email")
     expect(page).to have_selector("#email_response")
     expect(page).to have_current_path(root_path)
   end

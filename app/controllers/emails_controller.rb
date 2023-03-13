fedcda81097df9da530160d0050878d31c9a7fed
Bodil Hundevad <bodil@hundevad.de>
2023-03-13 10:17:13 +0100

    Add new and create actions to EmailsController
    Create new email and generate response on
    successful save

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,13 @@
 class EmailsController < ApplicationController
+  def new
+    @email = Email.new
+  end
+
+  def create
+    email = Email.new(email_params)
+    if email.save
+      @response = EmailGenerator.new(email).generate_response
+    else # render alert
+    end
+  end
 end

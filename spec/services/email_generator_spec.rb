4263e2eaa546b24ba810cee44d9983783c33427d
Bodil Hundevad <bodil@hundevad.de>
2023-03-14 13:54:36 +0100

    Refactor spec for EmailGenerator
    Refactor spec to make code more readable and organized for EmailGenerator service.

--- spec/services/email_generator_spec.rb
+++ spec/services/email_generator_spec.rb
@@ -1,9 +1,57 @@
 require 'rails_helper'
 
-describe EmailGenerator do
- context "when making requests to API, response", :vcr do
-    let(:email) { build_stubbed :email }
+describe EmailGenerator, :aggregate_failures do
+  describe "generated prompt" do
+    let(:email) { create :email }
+    context "received" do
+      let(:received) { email.received }
+      it "is present" do
+        prompt = EmailGenerator.new(email).body[:prompt]
+        expect(prompt).to include(received)
+      end
+      it "is not present" do
+        email = create :email, :received_empty
+        prompt = EmailGenerator.new(email).body[:prompt]
+        expect(prompt).not_to include(received)
+        p prompt
+      end
+    end
+    context "description" do
+      let(:description) { email.description }
+      it "is present" do
+        prompt = EmailGenerator.new(email).body[:prompt]
+        expect(prompt).to include(description)
+      end
+      it "is not present" do
+        email = create :email, :description_empty
+        prompt = EmailGenerator.new(email).body[:prompt]
+        expect(prompt).not_to include(description)
+        p prompt
+      end
+    end
+    context "tonality" do
+      let(:tonality) { email.tonality }
+      it "is present" do
+        prompt = EmailGenerator.new(email).body[:prompt]
+        expect(prompt).to include(tonality)
+      end
+      it "is not casual" do
+        email = create :email, :tonality_business
+        prompt = EmailGenerator.new(email).body[:prompt]
+        expect(prompt).not_to include(tonality)
+        p prompt
+      end
+      it "is not present" do
+        email = create :email, :tonality_empty
+        prompt = EmailGenerator.new(email).body[:prompt]
+        expect(prompt).to include(tonality)
+      end
+    end
+  end
+
+  describe "when making requests to API, response", :vcr do
     let(:response) { EmailGenerator.new(email).call }
+    let(:email) { create :email }
 
     it "is HTTParty::Response" do
       expect(response).to be_an(HTTParty::Response)
@@ -14,5 +62,7 @@ describe EmailGenerator do
       expect(email_message).to be_a(String)
       expect(email_message).not_to be_empty
     end
+
   end
+
 end
